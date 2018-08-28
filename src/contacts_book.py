e40f38e5c6d9fa14e114e1921237008aec074b36
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-08-28 17:40:12 +0100

    Feature & Test: add contact to contacts book.

--- src/contacts_book.py
+++ src/contacts_book.py
@@ -2,3 +2,6 @@ class Contacts_book():
 
     def __init__(self):
         self.contacts = []
+
+    def add_contact(self, contact):
+        self.contacts.append(contact)
--- tests/test_contacts_book.py
+++ tests/test_contacts_book.py
@@ -4,9 +4,16 @@ import sys
 sys.path.insert(0, '../src/')
 
 from contacts_book import *
+from contact import *
 
 class TestContactsBook():
 
     def test_contacts_book_has_contacts_list(self):
         contacts_book = Contacts_book()
         assert len(contacts_book.contacts) == 0
+
+    def test_contacts_book_adds_contact(self):
+        contacts_book = Contacts_book()
+        contact = Contact("Justyna", 123456)
+        contacts_book.add_contact(contact)
+        assert contacts_book.contacts[0].name == "Justyna"
