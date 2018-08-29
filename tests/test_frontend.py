36dcdef0e0784646e7c4a90924f41ec20aec9084
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-08-29 13:59:04 +0100

    Test: front end test for adding a contact and seing it on website.

--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -24,3 +24,16 @@ class TestFrontEnd(unittest.TestCase):
             driver.find_element_by_id("single_contact")
         except:
             self.fail("There are no contacts on the website")
+
+    def test_add_contact_to_contacts(self):
+        driver = webdriver.Chrome("/usr/local/bin/chromedriver")
+        driver.set_page_load_timeout(10)
+        driver.get("http://127.0.0.1:5000/")
+        driver.find_element_by_id("form-name").send_keys("Kota")
+        driver.find_element_by_id("form-telephone").send_keys("123")
+        driver.find_element_by_id("submit").click()
+
+        try:
+            driver.getPageSource().contains("Kota")
+        except:
+            self.fail("This contact is not visible on the website")
