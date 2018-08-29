9e3e5d65dcea41bdffdd93ad6a33e677652b240d
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-08-29 14:55:38 +0100

    Feature: implement adding contacts.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sys
 
 sys.path.insert(0, 'src')
@@ -15,6 +15,14 @@ contacts_book.add_contact(contact)
 @app.route('/')
 def index():
     return render_template('home.html', contacts = contacts_book.get_contacts())
+
+@app.route('/add', methods=['POST'])
+def addContact():
+    name = request.form['name']
+    telephone = request.form['telephone']
+    contact = Contact(name, telephone)
+    contacts_book.add_contact(contact)
+    return render_template('home.html', contacts = contacts_book.get_contacts(), message = "Success")
 
 if __name__ == '__main__':
     app.run(debug=True)
