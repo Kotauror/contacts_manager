3630bee2a541e65df741099a13a04828fcead84b
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-08-29 10:13:24 +0100

    Feature & Test: ContactsBook returns all contacts.

--- src/contacts_book.py
+++ src/contacts_book.py
@@ -1,7 +1,10 @@
-class Contacts_book():
+class ContactsBook():
 
     def __init__(self):
         self.contacts = []
 
     def add_contact(self, contact):
         self.contacts.append(contact)
+
+    def get_contacts(self):
+        return self.contacts
--- tests/test_contacts_book.py
+++ tests/test_contacts_book.py
@@ -19,3 +19,14 @@ class TestContactsBook():
         contacts_book.add_contact(contact)
 
         assert contacts_book.contacts[0].name == "Justyna"
+
+    def test_contacts_book_returns_all_contacts(self):
+        contacts_book = ContactsBook()
+        contact1 = Contact("Justyna", 123456)
+        contact2 = Contact("Igor", 987654)
+        contacts_book.add_contact(contact1)
+        contacts_book.add_contact(contact2)
+        contacts = contacts_book.get_contacts()
+
+        print(isinstance(contacts, list))
+        assert len(contacts) == 2
