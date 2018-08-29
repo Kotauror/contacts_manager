8316e70584355be08230ac68f55d47935a55c131
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-08-29 10:54:11 +0100

    Spike: contact visible on the website.

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+import sys
+
+sys.path.insert(0, 'src')
+
+from contacts_book import *
+from contact import * 
 
 app = Flask(__name__)
 
+contacts_book = ContactsBook()
+contact = Contact("Justyna", 123)
+contacts_book.add_contact(contact)
+
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('home.html', contacts = contacts_book.get_contacts())
 
 if __name__ == '__main__':
     app.run(debug=True)
