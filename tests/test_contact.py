5972e5abac935bdbd01f26e7b4559d6af1da07dd
Justyna Zygmunt <justynazygmunt@Justynas-MacBook-Air.local>
2018-09-03 11:19:03 +0200

    Fix: fix indentation.

--- tests/test_contact.py
+++ tests/test_contact.py
@@ -9,15 +9,13 @@ from contact import *
 class TestContact():
 
     def test_contact_has_name(self):
-        stubForm = ImmutableMultiDict([('name', 'Justyna'), ('telephone',
-                                                             '123456')])
+        stubForm = ImmutableMultiDict([('name', 'Justyna'), ('telephone', '123456')])
         contact = Contact(stubForm)
 
         assert contact.name == "Justyna"
 
     def test_contact_has_telephone(self):
-        stubForm = ImmutableMultiDict([('name', 'Justyna'), ('telephone',
-                                                             '123456')])
+        stubForm = ImmutableMultiDict([('name', 'Justyna'), ('telephone', '123456')])
         contact = Contact(stubForm)
 
         assert contact.telephone == '123456'
