9f98742234810c7dd5d9d4f8189cabf9870c3cf6
Justyna Zygmunt <justynazygmunt@justynas-air.home>
2018-09-12 20:37:37 +0100

    remove files moved to a server folder.

--- settings.py
+++
@@ -1,8 +0,0 @@
-import os
-import config
-from create_app import create_app
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = create_app(config.DevelopmentConfig)
-db = SQLAlchemy(app)
