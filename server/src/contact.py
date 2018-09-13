0ea2d2a652c1bab7f78872a625446d9f4c45492c
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-09-13 11:58:29 +0100

    Fix: fix representation (__repr__) in Contact.

--- server/src/contact.py
+++ server/src/contact.py
@@ -7,7 +7,7 @@ class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
     telephone = db.Column(db.String(120), unique=True)
-    __table_args__ = {'extend_existing': True} 
+    __table_args__ = {'extend_existing': True}
 
 
     def __init__(self, name, telephone):
@@ -15,4 +15,4 @@ class Contact(db.Model):
         self.telephone = telephone
 
     def __repr__(self):
-        return '<User %r>' % self.name
+        return '<Contact %r>' % self.name
