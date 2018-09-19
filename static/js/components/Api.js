da32c3e7f3d21113e72f90364a0641c649792b6e
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-09-19 14:17:55 +0100

    Refactor: extract fetch (post method) to Api module.

--- static/js/components/Api.js
+++ static/js/components/Api.js
@@ -2,11 +2,21 @@ class Api {
 
   static getContacts() {
     return fetch('/contacts')
-      .then((response) => {
-        if(response.ok) {
-          return response.json()
-        }
+      .then((response) => { return response.json() })
+  }
+
+  static addContact(name, telephone) {
+    return fetch('/contacts/add', {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "name": name,
+        "telephone": telephone,
       })
+    }).then((response) => { return response.json() })
   }
 
 }
--- static/js/components/App.react.js
+++ static/js/components/App.react.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Contact from './Contact.react';
 import ContactForm from './ContactForm.react';
-
 import Api from './Api';
 
 
