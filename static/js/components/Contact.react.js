726b54569f264233ea6bad1952fe1402895c32f7
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-09-26 09:26:18 +0100

    Update: move delete contact button to a separate component.

--- static/js/components/Contact.react.js
+++ static/js/components/Contact.react.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Api from './Api';
+import DeleteContact from './DeleteContact.react'
 
 class Contact extends React.Component {
 
   render() {
     return (
       <div>
-        { this.props.name }
-        { this.props.telephone }
-        <button onClick={(e) => this.handleDelete(e)} id='delete_button'> ❌ </button>
+        { this.props.name }, { this.props.telephone }
+        <DeleteContact
+          onClick={(e) => this.handleDelete(e)}
+        />
       </div>
     )
   }
