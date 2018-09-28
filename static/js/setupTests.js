2f03d703c65c6c7230c440ad0456a02c2611e5a7
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-09-28 11:06:49 +0100

    Rename: renamed static folder to frontend folder.

--- static/js/setupTests.js
+++
@@ -1,5 +0,0 @@
-// import Enzyme from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
-// import requestAnimationFrame from './shim';
-//
-// Enzyme.configure({ adapter: new Adapter(), disableLifecycleMethods: true });
